11f986bce10de83a48ef5c662727d8d7dff05e5e
Olushi Luqman O <olushilukmon03@gmail.com>
2018-11-29 07:11:02 +0100

    [Chore #162267565] Properly setup npm build scripts (#8)
    [Finishes #162267565]

--- src/db/config/config.js
+++ src/db/config/config.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const {
-  DB_USER, DB_PASSWORD, DB_NAME, DB_HOST, DB_DIALECT, DATABASE_URL
+  DB_USER, DB_PASSWORD, DB_NAME, DB_HOST, DB_DIALECT
 } = process.env;
 
 module.exports = {
@@ -15,13 +15,10 @@ module.exports = {
     dialect: DB_DIALECT
   },
   test: {
-    username: 'root',
-    password: null,
-    database: 'database_test',
-    host: '127.0.0.1',
-    dialect: 'mysql'
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres'
   },
   production: {
-    use_env_variable: DATABASE_URL
+    use_env_variable: 'DATABASE_URL'
   }
 };
